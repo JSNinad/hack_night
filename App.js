02461e8213a6ee4a87072b79125eb8eacfac61c9
JSNinad <102415351+JSNinad@users.noreply.github.com>
2023-10-29 10:14:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import FinanceSearchComponent from './components/finance';
+
+
+
+ 
+
+
+function App() {
+  return (
+    <div>
+      <FinanceSearchComponent/>
+
+      
+     
+      
+      
+    </div>
+  );
+}
+
+export default App;
